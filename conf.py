25c8e0f7e981e7b9867c0ff52bcbb9156d6304ff
Ramona Walls <rlwalls2008@gmail.com>
2019-10-08 14:24:35 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,9 +9,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'YOUR PROJECT NAME HERE as topic_tutorial'
+project = 'Git for Mere Mortals as topic_tutorial'
 copyright = '2019, CyVerse'
-author = 'your_name'
+author = 'Meghan Balk', 'Ramona L. Walls'
 version = '1.0'
 release = '1.0'
 
