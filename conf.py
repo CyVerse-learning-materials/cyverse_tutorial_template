a01432db465eb3d6ea65345681548298d8e1e3f5
Jason Williams <JasonJWilliamsNY@gmail.com>
2020-05-19 11:45:44 -0400

    fix conf.py

--- conf.py
+++ conf.py
@@ -10,11 +10,11 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'Git for Mere Mortals as topic_tutorial'
-copyright = '2019, CyVerse'
-author = 'Meghan Balk', 'Ramona L. Walls'
-version = '1.0'
-release = '1.0'
+project = 'YOUR PROJECT NAME HERE as topic_tutorial'
+copyright = '2020, CyVerse'
+author = 'CyVerse'
+version = '2.0'
+release = '2.0'
 
 epub_title = project
 epub_author = author
